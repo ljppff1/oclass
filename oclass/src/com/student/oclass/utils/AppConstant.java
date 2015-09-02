017085ccefbb66ebeae07a0beb0501068090f819
liujun <ljppff@163.com>
2015-09-02 12:09:00 +0800

    what amazing

---
+++ oclass/src/com/student/oclass/utils/AppConstant.java
@@ -0,0 +1,22 @@
+package com.student.oclass.utils;
+public class AppConstant {
+
+	public static final String EMAIL_KEY="email_key";
+	public static final String APP_HAS_UPDATE_KEY="has_update";
+	public static final String APP_UPDATE_URL="update_url";
+	public static final String APP_UPDATE_REMARK="update_remark";
+	public static final String SCORE="score";
+	public static final String BEST_SCORE="best_score";
+	public static final boolean DEBUG = false;
+	public static final int FAMOUS_BOOK=100;
+	public static final int BEST_HEAD=101;
+	public static final int EXAM=102;
+	public static final boolean DEV_MODE = false;
+	//这些图片不存在，根据实际需求替换为自己的
+	public static String[] imgsUrl = {
+		"http://www.sinaimg.cn/dy/slidenews/4_img/2015_33/704_1709459_549377.jpg",
+		"http://www.sinaimg.cn/dy/slidenews/4_img/2015_33/704_1709459_549377.jpg",
+		"http://www.sinaimg.cn/dy/slidenews/4_img/2015_33/704_1709459_549377.jpg",
+		"http://www.sinaimg.cn/dy/slidenews/4_img/2015_33/704_1709459_549377.jpg",
+};
+}
